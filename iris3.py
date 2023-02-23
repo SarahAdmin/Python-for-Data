3e65e2801300944398cc6114f64f994befc5e644
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2023-02-23 10:22:54 +0000

    Create iris3.py

---
+++ iris3.py
@@ -0,0 +1,5 @@
+from sklearn.datasets import load_iris # load the iris function
+# run the load_iris()function and save the return value in an object called # iris
+iris = load_iris() 
+print(iris.target)
+print(iris.target_names)
